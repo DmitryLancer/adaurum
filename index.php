0f09e733f5782f94e1b11efac4e943e624e90315
Dmitry <dmitrylancer.it@yandex.ru>
2023-02-26 14:31:13 +0300

    add login and register form

--- index.php
+++ index.php
@@ -42,6 +42,33 @@ $app->get('/', function (Request $request, Response $response, $args) use ($view
     return $response;
 });
 
+$app->get('/login', function (Request $request, Response $response) use ($view) {
+    $body = $view->render('section/login.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->post('/login-post', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $response->getBody()->write('Login Page!');
+    return $response;
+});
+
+$app->get('/register', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $body = $view->render('section/register.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->post('/register-post', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $response->getBody()->write('Register Page!');
+    return $response;
+});
+
+$app->get('/logout', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $response->getBody()->write('Logout Page!');
+    return $response;
+});
+
 $app->get('/about', function (Request $request, Response $response, $args) use ($view) {
     $body = $view->render('about.twig', [
         'name' => 'Dmitry'
